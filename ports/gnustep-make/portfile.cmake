05bfeaa3afb47cf802b3aefe992f6c673ced4de5
Frederik Carlier <frederik.carlier@keysight.com>
2024-06-28 13:26:00 +0200

    Have greater control over ./configure, make and make install

--- ports/gnustep-make/portfile.cmake
+++ ports/gnustep-make/portfile.cmake
@@ -9,33 +9,57 @@ vcpkg_from_github(
     PATCHES
 )
 
-vcpkg_list(SET OPTIONS)
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_acquire_msys(MSYS_ROOT
-        PACKAGES
-            bash
-    )
-    vcpkg_add_to_path("${MSYS_ROOT}/usr/bin")
-    vcpkg_list(APPEND OPTIONS "-DBASH_EXECUTABLE=${MSYS_ROOT}/usr/bin/bash.exe")
-endif()
-
 # Get the Windows 10 include location (which we need to pass to MSYS)
 get_filename_component(WINKIT_ROOT "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot10]" ABSOLUTE CACHE)
 file(GLOB WINKIT_LIB_CANDIDATES "${WINKIT_ROOT}/Lib/*")
 list(GET WINKIT_LIB_CANDIDATES 0 WINKIT_LIB_CANDIDATE)
 get_filename_component(WINKIT_LIB "${WINKIT_LIB_CANDIDATE}/um/x64" ABSOLUTE CACHE)
-# # Escape spaces
-# string(REPLACE " " "\\ " WINKIT_LIB "${WINKIT_LIB}")
-# # Fix path
-# string(REGEX REPLACE "^([a-zA-Z]):/" "/\\1/" WINKIT_LIB "${WINKIT_LIB}")
 
-#set(ENV{LDFLAGS} "-fuse-ld=lld -L\'${WINKIT_LIB}\'")
+vcpkg_list(SET OPTIONS)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_acquire_msys(MSYS_ROOT
+        PACKAGES
+            bash
+            autoconf-wrapper
+            automake-wrapper
+            binutils
+            libtool
+            make
+            pkgconf
+            which
+    )
 
-vcpkg_configure_make(
-    AUTOCONFIG
-    OPTIONS --target=x86_64-pc-windows --host=x86_64-pc-windows LDFLAGS=\"-fuse-ld=lld -L\"${WINKIT_LIB}\"\"
-    SOURCE_PATH ${SOURCE_PATH}
-)
+    z_vcpkg_get_cmake_vars(cmake_vars_file)
+    debug_message("Including cmake vars from: ${cmake_vars_file}")
+    include("${cmake_vars_file}")
 
-vcpkg_build_make(BUILD_TARGET compile)
-vcpkg_build_make(BUILD_TARGET install)
+    set(base_cmd "${MSYS_ROOT}/usr/bin/bash.exe" --noprofile --norc --debug)
+
+    vcpkg_list(APPEND path_list "${MSYS_ROOT}/usr/bin")
+
+    get_filename_component(LLVM_PATH ${VCPKG_DETECTED_CMAKE_C_COMPILER} DIRECTORY)
+    vcpkg_list(APPEND path_list "${LLVM_PATH}")
+
+    cmake_path(CONVERT "${path_list}" TO_NATIVE_PATH_LIST native_path_list)
+    set(ENV{PATH} "${native_path_list}")
+    message("Using path ${native_path_list}")
+
+    vcpkg_execute_required_process(
+        COMMAND ${base_cmd} -c "./configure CC=clang.exe CXX=clang++.exe"
+        WORKING_DIRECTORY "${SOURCE_PATH}"
+        LOGNAME "config-${TARGET_TRIPLET}-${short_name_${current_buildtype}}"
+        SAVE_LOG_FILES config.log
+    )
+    
+    vcpkg_execute_required_process(
+        COMMAND ${base_cmd} -c "make"
+        LOGNAME "make-${TARGET_TRIPLET}-${short_name_${current_buildtype}}"
+        WORKING_DIRECTORY "${SOURCE_PATH}"
+    )
+    
+    vcpkg_execute_required_process(
+        COMMAND ${base_cmd} -c "make install"
+        LOGNAME "install-${TARGET_TRIPLET}-${short_name_${current_buildtype}}"
+        WORKING_DIRECTORY "${SOURCE_PATH}"
+    )
+endif()
