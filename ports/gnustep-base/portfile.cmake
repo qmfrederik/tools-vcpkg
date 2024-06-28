59506f786ccd92e46c0ac4a44965e8ddf21fa044
Frederik Carlier <frederik.carlier@keysight.com>
2024-06-28 17:07:56 +0200

    Prototype gnustep-base (up to libffi)

--- ports/gnustep-base/portfile.cmake
+++ ports/gnustep-base/portfile.cmake
@@ -2,11 +2,13 @@ string(REPLACE "." "_" MAKE_VERSION ${VERSION})
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO gnustep/tools-make
-    REF "make-${MAKE_VERSION}"
-    SHA512 ec1a21a36cd39d354dc1ed88e2c0b576ae5418af562f6ecb66619442d967a22b0eb7dee9914cfe4430674ca0c409d5755df76308a7c90af64ab9dbeaf9b85b28
+    REPO gnustep/libs-base
+    REF "base-${MAKE_VERSION}"
+    SHA512 f656ad73138e476874fa70c5fa74718b023e97314e80d3a75ec7f25efe90d11a8dd6dd5d18706797e7be590f53300e9adb031bba3c85fdf9fd909dbf0d57b08e
     HEAD_REF master
     PATCHES
+        # Workaround for https://github.com/llvm/llvm-project/issues/49025
+        llvm-bug.patch
 )
 
 vcpkg_list(SET OPTIONS)
@@ -33,10 +35,6 @@ if(VCPKG_TARGET_IS_WINDOWS)
 
     get_filename_component(LLVM_PATH ${VCPKG_DETECTED_CMAKE_C_COMPILER} DIRECTORY)
     vcpkg_list(APPEND path_list "${LLVM_PATH}")
-
-    cmake_path(CONVERT "${path_list}" TO_NATIVE_PATH_LIST native_path_list)
-    set(ENV{PATH} "${native_path_list}")
-    message("Using path ${native_path_list}")
 
     # Cleanup previous build dirs
     file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-${short_name_RELEASE}"
@@ -65,11 +63,16 @@ if(VCPKG_TARGET_IS_WINDOWS)
                         "--datarootdir=\\\${prefix}/share/${PORT}"
                         "--host=x86_64-pc-windows"
                         "--target=x86_64-pc-windows"
-                        "--with-library-combo=ng-gnu-gnu"
-                        "--with-runtime-abi=gnustep-2.2"
                         "CC=clang.exe"
                         "CXX=clang++.exe"
                         "LDFLAGS=-fuse-ld=lld")
+
+    # Add gnustep-make to path so we can run gnustep-config
+    vcpkg_list(APPEND path_list "${CURRENT_INSTALLED_DIR}/${path_suffix}/bin")
+
+    cmake_path(CONVERT "${path_list}" TO_NATIVE_PATH_LIST native_path_list)
+    set(ENV{PATH} "${native_path_list}")
+    message("Using path ${native_path_list}")
 
     list(JOIN CONFIGURE_OPTIONS " " CONFIGURE_OPTIONS)
 
