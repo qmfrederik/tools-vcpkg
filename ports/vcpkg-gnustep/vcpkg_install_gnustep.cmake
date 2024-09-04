8534e2a29ef11d2a4f4ed7e35bc62db5256b6e87
Frederik Carlier <frederik.carlier@keysight.com>
2024-09-04 12:00:30 +0200

    Try workaround for install options

--- ports/vcpkg-gnustep/vcpkg_install_gnustep.cmake
+++ ports/vcpkg-gnustep/vcpkg_install_gnustep.cmake
@@ -7,8 +7,14 @@ function(vcpkg_install_gnustep)
         "OPTIONS"
     )
 
+    list(JOIN ${arg_OPTIONS} " " options_string)
+
     if (VCPKG_TARGET_IS_LINUX OR VCPKG_TARGET_IS_WINDOWS)
-        vcpkg_install_make(
+        vcpkg_build_make(
+            ENABLE_INSTALL
+            # vcpkg_build_make passes OPTIONS to make [build] but not to make [install]; try to squeeze them in via
+            # the install target instead
+            INSTALL_TARGET install ${options_string}
             MAKEFILE GNUmakefile
             # Allow make to find gnustep-config, which is in bin/
             ADD_BIN_TO_PATH
